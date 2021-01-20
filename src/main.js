e655a0080a64c7648c77220c436444fbcb9b9b7c
Zachary Fuller <zachary.fuller.dev@gmail.com>
2021-01-20 14:48:44 -0500

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -3,17 +3,27 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { BootstrapVueIcons } from 'bootstrap-vue'
+
 import Vuesax from 'vuesax'
 import 'vuesax/dist/vuesax.css' //Vuesax styles
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { faPepperHot } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import VueAnalytics from 'vue-analytics';
+
+Vue.use(VueAnalytics, {
+  id: "UA-187832019-1",
+  router
+})
 
 Vue.use(BootstrapVueIcons)
 Vue.use(BootstrapVue)
